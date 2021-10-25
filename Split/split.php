addbfe1be6e62e9b45c6624cc8eac17f29c28516
Valerie Pond <79415174+ValwareIRC@users.noreply.github.com>
2021-10-25 04:07:54 +0100

    Create split.php

---
+++ Split/split.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+
+<?php
+
+$toSplit = "Hello world!";
+
+$split = explode(" ",$toSplit);
+
+echo "This is the first word: ".$split[0].", and this is the second: ".$split[1]."\n";
+
+?>
