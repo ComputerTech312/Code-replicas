0c486ac74eb33e0be774342b2f1e7028277ce42f
ComputerTech <60820431+ComputerTech312@users.noreply.github.com>
2021-10-27 00:37:39 +0100

    Create if.java

---
+++ If/if.java
@@ -0,0 +1,31 @@
+public class number_greater_than_100 {
+
+    public static void main(String[] args) {
+
+        int number=106;
+
+        if(number>0){
+
+            if (number>100){
+
+                System.out.println("Number is greater than 100");
+
+            }
+
+            else {
+
+                System.out.println("Number is less than 100");
+
+            }
+
+        }
+
+        else{
+
+            System.out.println("Invalid Input");
+
+        }
+
+    }
+
+}
