fd50543bd8280be058eb8b29f2a9fcbdb80a4446
ComputerTech <60820431+ComputerTech312@users.noreply.github.com>
2021-10-26 16:01:32 +0100

    update split.c

--- Split/split.c
+++ Split/split.c
@@ -8,6 +8,5 @@ int main ()
   char *a = "a!b";
   char *b = strtok(a,"!";
   printf(b[0] "is the first argument, and" + b[1] + "is the second."\n);
-  print(b[0] + "is the first Argument, and" + b[1] + "is the second.")
   return 0;
 }
