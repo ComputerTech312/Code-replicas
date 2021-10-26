8359f116168dbea5c0820231f467f27d4f41967c
ComputerTech <60820431+ComputerTech312@users.noreply.github.com>
2021-10-26 20:00:08 +0100

    Update split.c

--- Split/split.c
+++ Split/split.c
@@ -1,12 +1,13 @@
-#!/usr/bin/gcc
-
 #include <stdio.h>
 #include <string.h>
 
 int main ()
 {
-  char *a = "a!b";
-  char *b = strtok(a,"!");
-  printf("%s is the first argument, and %s is the second",b[0],b[1]);
+  char a[] = "foo!bar";
+  char *first = strtok(a, "!");
+  char *second = strtok(NULL, "!");
+
+  printf("%s is the first argument, and %s is the second\n", first, second);
+
   return 0;
 }
