48c26ac7f66b2feb81940b5932e97a48753afff2
Valerie Pond <79415174+ValwareIRC@users.noreply.github.com>
2021-12-30 04:35:54 +0000

    Update if.c

--- If/if.c
+++ If/if.c
@@ -1,8 +1,14 @@
-int a = 100
+#include <stdio.h>
+
+int main()
+{
+  int a = 100
   
-if (a > 100) {
-  printf("It's greater than 100!")
-}
-else {
-  printf("It's less than 100!")
+  if (a > 100) {
+    printf("It's greater than 100!")
+  }
+  else {
+   printf("It's less than 100!")
+  }
+  return 0;
 }
